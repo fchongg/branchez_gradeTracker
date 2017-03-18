4ff89e93b8f13f824bb41d274434c123e91aed35
fchongg <fchongg@gmail.com>
2017-03-18 15:59:47 -0700

    added db

--- gradetracker/courses/models.py
+++ gradetracker/courses/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    cid = models.IntegerField()
+    cname = models.CharField(max_length=8)
+    term = models.CharField(max_length=2)
+
+class AgType(models.Model):
+	agtid = models.IntegerField()
+	agname = models.CharField(max_length=100)
+
+class AssessmentGroup(models.Model):
+    cid = models.ForeignKey(Course, on_delete=models.CASCADE)
+    agid = models.IntegerField()
+    agpercentage = models.PositiveSmallIntegerField()
+    agtid = models.ForeignKey(AgType)
+
+
+class Assessment(models.Model):
+	agtid = models.ForeignKey(AgType)
+	aname = models.CharField(max_length=200)
+	agid = models.ForeignKey(AssessmentGroup)
+	apercentage = models.PositiveSmallIntegerField()
+
