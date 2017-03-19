6c2487733ef48b39da92edce99c5d959f61729a8
fchongg <fchongg@gmail.com>
2017-03-18 22:36:19 -0700

    frances' commit

--- gradetracker/gradetracker/urls.py
+++ gradetracker/gradetracker/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     url(r'^courses/', include('courses.urls')),
     url(r'^home/', include('home.urls')),
+    url(r'^', include('register.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
 ]
