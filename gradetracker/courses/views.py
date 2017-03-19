04877b37761815a8499dd7dc81e50928ce1ff843
fchongg <fchongg@gmail.com>
2017-03-18 17:26:16 -0700

    view for users

--- gradetracker/courses/views.py
+++ gradetracker/courses/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpResponse
 
+from .models import Course
+from .models import AssessmentGroup
+from .models import Assessment
+
 
 def index(request):
     return HttpResponse("Hello, you're at the courses section")
+
+def courses(request, question_id):
+	current_user = request.user
+	if current_user.is_authenticated():
+		# grab all the courses associated with that id
+		all_courses = Course.object.get(uid=current_user.id)
+	else: 
+		# do nothing
+    return HttpResponse("You're looking at question %s." % question_id)
