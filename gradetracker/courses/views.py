d91a302d05aedc4ab847192f352fceea1a4f9520
fchongg <fchongg@gmail.com>
2017-03-18 19:00:23 -0700

    resolved p2 or merge conflict

--- gradetracker/courses/views.py
+++ gradetracker/courses/views.py
@@ -16,15 +16,6 @@ def courseDetail(request):
     return HttpResponse();
 
 def courses(request):
-<<<<<<< HEAD
-
-
-
-    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # context = {'latest_question_list': latest_question_list}
-    # return render(request, 'polls/index.html', context)
-
-
 
 	current_user = request.user
 	# template = loader.get_template('course')
@@ -32,19 +23,8 @@ def courses(request):
 
 	context = {
        	'all_courses': all_courses,
-
    	}
-	# 	if(all_courses == null):
-	# 		#TODO: empty context here
-	# else: 
-	# 	# do nothing
-
 	return render(request, 'courses/course.html', context)
-=======
-
-
-	return HttpResponse("courses")
->>>>>>> 33bb2a8ba4dc320cf345d661ef65a33a5f8b61ba
 
 def addcourses(request):
 
