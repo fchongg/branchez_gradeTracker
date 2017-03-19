fa0a90ecf90dcf8c7bf2c5a0a96d8461eb380ebc
Tracey Lui <tracey@Traceys-MacBook-Pro.local>
2017-03-18 22:34:48 -0700

    committing stuff

--- gradetracker/courses/views.py
+++ gradetracker/courses/views.py
@@ -2,17 +2,31 @@ from django.http import HttpResponse
 
 from .models import Course
 from .models import AssessmentGroup
-from .models import Assessment
+from .models import Assessment, AgType
 from django.template import loader
+from django.shortcuts import render
+from itertools import chain
 
 
 def index(request):
     return HttpResponse("Hello, you're at the courses section")
 
-
-def courseDetail(request):
-
-    return HttpResponse();
+def courseDetail(request, course_id):
+	assignmentgroups = AssessmentGroup.objects.filter(cid = course_id)
+	courseName = Course.objects.get(id = course_id).cname
+	assignments = []
+	groupedAssignments = []
+	for ag in assignmentgroups:
+		assignGroupObj = {}
+		t = AgType.objects.get(id=ag.agtid.id).agname
+		assignGroupObj[t] = list(Assessment.objects.filter(agid = ag.id).order_by('-duedate'))
+		groupedAssignments.append(assignGroupObj)
+		assignments = assignments + list(Assessment.objects.filter(agid = ag.id))
+	context = {
+		'assignments' : assignments,
+		'courseName': courseName
+	}
+	return render(request, 'courses/coursedetails.html',context)
 
 def courses(request, question_id):
 	current_user = request.user
@@ -26,5 +40,6 @@ def courses(request, question_id):
 
 def addCourses(request):
     return HttpResponse("Hello, you're at the add courses page.")
+
 
 
