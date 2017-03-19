83c12f951a5ba6a02b413c16e4863e019fcfb941
fchongg <fchongg@gmail.com>
2017-03-18 18:52:44 -0700

    Added courses main page

--- gradetracker/courses/views.py
+++ gradetracker/courses/views.py
@@ -4,6 +4,7 @@ from .models import Course
 from .models import AssessmentGroup
 from .models import Assessment
 from django.template import loader
+from django.shortcuts import render
 
 
 def index(request):
@@ -14,19 +15,30 @@ def courseDetail(request):
 
     return HttpResponse();
 
-def courses(request, question_id):
+def courses(request):
+
+
+
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # context = {'latest_question_list': latest_question_list}
+    # return render(request, 'polls/index.html', context)
+
+
+
 	current_user = request.user
-	template = loader.get_template('course')
+	# template = loader.get_template('course')
+	all_courses = Course.objects.filter(uid=1)
 
-	if current_user.is_authenticated():
-		# grab all the courses associated with that id
-		all_courses = Course.object.get(uid=current_user.id)
-		if(all_courses == null):
-			#TODO: empty context here
-	else: 
-		# do nothing
+	context = {
+       	'all_courses': all_courses,
 
-	return HttpResponse(template.render(_,request))
+   	}
+	# 	if(all_courses == null):
+	# 		#TODO: empty context here
+	# else: 
+	# 	# do nothing
+
+	return render(request, 'courses/course.html', context)
 
 def addCourses(request):
     return HttpResponse("Hello, you're at the add courses page.")
